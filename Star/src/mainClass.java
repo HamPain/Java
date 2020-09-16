9108adb0d37eca04b7e63d23766f4bf9eaadbc99
HamPain <42128097+HamPain@users.noreply.github.com>
2020-09-17 03:04:27 +0900

    별찍기

---
+++ Star/src/mainClass.java
@@ -0,0 +1,18 @@
+
+public class mainClass {
+	static int i;
+	static int j;
+	public static void main(String[] args) {
+		
+			for(i = 0 ; i <= 5 ; i++) {
+			
+					for(j = 0 ; j <= i  ; j++) {
+					System.out.print("*");
+					}
+					System.out.println("");
+			}
+				
+		}
+
+	}
+//º°Âï±â
